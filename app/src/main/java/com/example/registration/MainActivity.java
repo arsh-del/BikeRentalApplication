eb77f53fc059330e0e45471f595548c25bd30520
Arshdeep <itsrandhawaasr@gmail.com>
2020-08-22 09:39:03 -0700

    Done

--- app/src/main/java/com/example/registration/MainActivity.java
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -5,27 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import static java.lang.Thread.sleep;
 
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn;
+    ImageView splashImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        splashImage = findViewById( R.id.splashview );
 
-        btn=findViewById(R.id.btnlooooo);
+        Animation myanim = AnimationUtils.loadAnimation( this,R.anim.myanimation );
+        splashImage.startAnimation( myanim );
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        Thread mythread = new Thread( new Runnable() {
             @Override
-            public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, Login.class);
-                startActivity(i);
+            public void run () {
+                try {
+                    sleep(3000);
+                    Intent i = new Intent(MainActivity.this,Login.class );
+                    startActivity( i );
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
             }
-        });
+        } );
+        mythread.start();
     }
 }
+
